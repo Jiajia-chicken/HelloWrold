898bf726c0459e56a333d07bb17ae3ae4b41ff4f
tyh <143810091+howdnb@users.noreply.github.com>
2023-11-26 11:50:44 +0800

    usemodels

--- dbscripts/models.py
+++ dbscripts/models.py
@@ -1,17 +1,41 @@
-#建立数据库
-from exts import db
-from datetime import datetime
+from app import db
+from config import get_cur_time, get_md5,user_status_true,user_status_false, NoticeRead, Echarts
 
-class UserModel(db.Model):
-    __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    join_time = db.Column(db.DateTime, default=datetime.now)
+# 用户表
+class User(db.Model):  # 创建的类对应数据库的表以及对表的相关操作
+    __tablename__ = 'user'
+    __table_args__ = {
+        'autoload': True,
+        'autoload_with': db.engine
+    }
 
-class EmailCaptchaModel(db.Model):
-    __tablename__ = "email_captcha"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    captcha = db.Column(db.String(100), nullable=False)
+    @classmethod
+    def user_login(self, account, pwd):  # 用户登录
+        query_user = db.session.query(User)
+        all_user = query_user.all()
+        flag = False
+        user_id = 0
+        username = ''
+        identity = ''
+        user_status = ''
+        create_time = ''
+        for user in all_user:
+            if user.account == account and user.password == get_md5(pwd):
+                user_id = user.id
+                username = user.username
+                identity = user.identity
+                user_status = user.user_status
+                create_time = user.create_time
+                break
+        if user_status != user_status_true: return {}
+        flag = True
+        if flag == True:
+            flag = False
+            login_time = get_cur_time()
+            # 更新用户登录时间
+            user = db.session.query(User).filter(User.account == account).first()
+            user.login_time = login_time
+            db.session.commit()
+            return {'user_id': user_id, 'username': username, 'identity': identity, 'user_status': user_status, 'create_time': create_time, 'login_time': login_time}
+        else:
+            return {}
